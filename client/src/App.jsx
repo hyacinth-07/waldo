827f6a22531d1096b372b30cb6d9bc20c13dc020
hyacinth-07 <ndr.hya37@gmail.com>
2024-04-02 15:48:27 +0200

    get rel coordinates

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,22 @@
 import testImage from '../src/assets/test_waldo.png';
 
 function App() {
+	function imageClick(e) {
+		const rect = e.currentTarget.getBoundingClientRect();
+		const xCoord = e.clientX - rect.left;
+		const yCoord = e.clientY - rect.top;
+		alert(`Click: X = ${xCoord}, Y = ${yCoord}`);
+	}
+
 	return (
 		<>
 			<div className="h-screen w-screen bg-slate-400 flex justify-center items-center">
-				<img src={testImage} alt="the testing image" className="w-fit h-fit" />
+				<img
+					src={testImage}
+					alt="the testing image"
+					className="w-fit h-fit min-w-fit min-h-fit"
+					onClick={imageClick}
+				/>
 			</div>
 		</>
 	);
