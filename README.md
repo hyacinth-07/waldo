b93a3496c4236239694c568f5e1776c5055ad0ae
hyacinth-07 <ndr.hya37@gmail.com>
2024-04-11 13:23:43 +0200

    readme

--- README.md
+++ README.md
@@ -4,6 +4,9 @@
 - server the backend (node js, express, mongodb)
 
 - after the game is fully functional, refactor everything into its own component
+- aka share state properly, read docs
+
+- create stageSelect UI, where the user can pick three stages to play
 
 - setTimeouts in UI suck. do something else
 - make it pretty, with animations and text that pops up creatively
--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState, useRef } from 'react';
-// import Timer from './components/Timer';
+import Timer from './components/Timer';
 
 function App() {
 	const [coord, setCoord] = useState('X: ??? | X: ???');
@@ -117,66 +117,6 @@ function App() {
 				</button>
 			);
 		}
-	}
-
-	// TIMER
-
-	function Timer() {
-		const [isRunning, setIsRunning] = useState(false);
-		const [elapsedTime, setElapsedTime] = useState(0);
-		const intervalRef = useRef(null);
-		const startTimeRef = useRef(0);
-
-		useEffect(() => {
-			if (isRunning) {
-				intervalRef.current = setInterval(() => {
-					setElapsedTime(Date.now() - startTimeRef.current);
-				}, 10);
-			}
-
-			return () => {
-				clearInterval(intervalRef.current);
-			};
-		}, [isRunning]);
-
-		function start() {
-			setIsRunning(true);
-			startTimeRef.current = Date.now() - elapsedTime;
-		}
-
-		function stop() {
-			setIsRunning(false);
-		}
-
-		function reset() {
-			setElapsedTime(0);
-			setIsRunning(false);
-		}
-
-		function format() {
-			let minutes = Math.floor((elapsedTime / (1000 * 60)) % 60);
-			let seconds = Math.floor((elapsedTime / 1000) % 60);
-			let milliSeconds = Math.floor((elapsedTime % 1000) / 10);
-
-			minutes = String(minutes).padStart(2, '0');
-			seconds = String(seconds).padStart(2, '0');
-			milliSeconds = String(milliSeconds).padStart(2, '0');
-
-			return `${minutes} : ${seconds} : ${milliSeconds}`;
-		}
-
-		return (
-			<>
-				<div>
-					<div>{format()}</div>
-					<div>
-						<button onClick={start}>Start</button>
-						<button onClick={stop}>Stop</button>
-						<button onClick={reset}>Reset</button>
-					</div>
-				</div>
-			</>
-		);
 	}
 
 	// startup init
