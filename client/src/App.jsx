ba134e54474c1db8dbcc242c936d38a740216728
hyacinth-07 <ndr.hya37@gmail.com>
2024-04-11 13:14:25 +0200

    a working timer

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,5 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
+// import Timer from './components/Timer';
 
 function App() {
 	const [coord, setCoord] = useState('X: ??? | X: ???');
@@ -118,6 +119,66 @@ function App() {
 		}
 	}
 
+	// TIMER
+
+	function Timer() {
+		const [isRunning, setIsRunning] = useState(false);
+		const [elapsedTime, setElapsedTime] = useState(0);
+		const intervalRef = useRef(null);
+		const startTimeRef = useRef(0);
+
+		useEffect(() => {
+			if (isRunning) {
+				intervalRef.current = setInterval(() => {
+					setElapsedTime(Date.now() - startTimeRef.current);
+				}, 10);
+			}
+
+			return () => {
+				clearInterval(intervalRef.current);
+			};
+		}, [isRunning]);
+
+		function start() {
+			setIsRunning(true);
+			startTimeRef.current = Date.now() - elapsedTime;
+		}
+
+		function stop() {
+			setIsRunning(false);
+		}
+
+		function reset() {
+			setElapsedTime(0);
+			setIsRunning(false);
+		}
+
+		function format() {
+			let minutes = Math.floor((elapsedTime / (1000 * 60)) % 60);
+			let seconds = Math.floor((elapsedTime / 1000) % 60);
+			let milliSeconds = Math.floor((elapsedTime % 1000) / 10);
+
+			minutes = String(minutes).padStart(2, '0');
+			seconds = String(seconds).padStart(2, '0');
+			milliSeconds = String(milliSeconds).padStart(2, '0');
+
+			return `${minutes} : ${seconds} : ${milliSeconds}`;
+		}
+
+		return (
+			<>
+				<div>
+					<div>{format()}</div>
+					<div>
+						<button onClick={start}>Start</button>
+						<button onClick={stop}>Stop</button>
+						<button onClick={reset}>Reset</button>
+					</div>
+				</div>
+			</>
+		);
+	}
+
 	// startup init
 
 	useEffect(() => {
@@ -129,6 +190,7 @@ function App() {
 		<>
 			<div className="h-screen w-screen bg-slate-400 flex flex-col justify-center items-center">
 				<div>
+					<Timer />
 					<img
 						src={bgImage}
 						alt="the testing image"
@@ -169,10 +231,12 @@ function App() {
 					</div>
 				</div>
 
-				<p>{coord}</p>
-				<p>{message}</p>
-				<div className="*:mx-2">
-					<button onClick={() => gameReset()}>Game Reset</button>
+				<div className="flex justify-around w-[500px]">
+					<p>{coord}</p>
+					<p>{message}</p>
+					<div className="*:mx-2">
+						<button onClick={() => gameReset()}>Game Reset</button>
+					</div>
 				</div>
 			</div>
 		</>
